a7782d098eccc6e50055ea783f9a11ea8bb60c10
Bruno Marques <brunoa879@gmail.com>
2025-11-12 20:41:52 -0300

    chore: bootstrap Spring Boot project (Web/Validation/JPA/H2/Actuator)

---
+++ riskguard/src/main/java/com/riskguard/riskguard/RiskguardApplication.java
@@ -0,0 +1,13 @@
+package com.riskguard.riskguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiskguardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiskguardApplication.class, args);
+	}
+
+}
---
+++ riskguard/src/test/java/com/riskguard/riskguard/RiskguardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riskguard.riskguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiskguardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
